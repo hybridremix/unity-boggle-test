6e410c59d5b4b0ea360b59fdcdb0d4d5360ab653
K. Paul Garrett <hybridremix@yahoo.com>
2023-01-05 15:47:08 -0500

    Initial grid size should be 4

--- Assets/Scripts/BoggleGlobals.cs
+++ Assets/Scripts/BoggleGlobals.cs
@@ -3,7 +3,7 @@
     static class BoggleGlobals
     {
         public static int NumBlockSides = 6;
-        public static int GridSize = 3;
+        public static int GridSize = 4;
         public static int MinWordLength = 3;
         public static int MaxWordLength = GridSize * GridSize;
     }
